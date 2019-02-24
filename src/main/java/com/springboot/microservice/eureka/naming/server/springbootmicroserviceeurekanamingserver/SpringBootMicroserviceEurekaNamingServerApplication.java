3cfb740d8cffc339e542c13924ba0858a2f682ff
Csriteja <csriteja.1110@gmail.com>
2019-02-24 23:20:56 +0530

    Eureka Naming Server

---
+++ src/main/java/com/springboot/microservice/eureka/naming/server/springbootmicroserviceeurekanamingserver/SpringBootMicroserviceEurekaNamingServerApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.microservice.eureka.naming.server.springbootmicroserviceeurekanamingserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootMicroserviceEurekaNamingServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootMicroserviceEurekaNamingServerApplication.class, args);
+	}
+
+}
